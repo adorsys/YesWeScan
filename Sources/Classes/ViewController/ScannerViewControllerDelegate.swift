582957e9245eefc5ca2eef1a7ee9e1b41bdb39bc
App4Mission <support@app4mission.de>
2019-03-17 14:04:14 +0100

    Update ScannerViewControllerDelegate.swift
    I add the @objc to the ScannerViewController Delegate Method to use the delegate method in a objective-c iOS app.

--- Sources/Classes/ViewController/ScannerViewControllerDelegate.swift
+++ Sources/Classes/ViewController/ScannerViewControllerDelegate.swift
@@ -1,5 +1,5 @@
 import UIKit
 
-public protocol ScannerViewControllerDelegate: AnyObject {
+@objc public protocol ScannerViewControllerDelegate: AnyObject {
     func scanner(_ scanner: ScannerViewController, didCaptureImage image: UIImage)
 }
