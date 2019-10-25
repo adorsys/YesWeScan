61fa459c5d0ac37ad72d7ee3aa7b78e0d1f6bd1f
Xavier Lowmiller <xaver.lohmueller@me.com>
2019-10-25 10:37:37 +0200

    Migrate to Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YesWeScan",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "YesWeScan",
+            targets: ["YesWeScan"]),
+    ],
+    targets: [
+        .target(
+            name: "YesWeScan",
+            dependencies: [])
+    ],
+    swiftLanguageVersions: [.v5]
+)
